a00424620c7bf94b03155f5ff16bec6f4a004486
Petr Ivliev <ivliev@inbox.ru>
2021-04-14 23:20:40 +0300

    2nd task completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,36 +1,14 @@
 "use strict";
-// function checkValues () {
-//   if (percent < 0) {
-//     return `Параметр ${percent} содержит неправильное значение ${percent}`;
-//   } else {
-//     calculateTotalMortgage();
-//   };
-  // if (contribution < 0 || contribution === 'string' || contribution === NaN) {
-  //   return `Параметр ${contribution} содержит неправильное значение ${contribution}`;
-  // } else {
-  //   calculateTotalMortgage();
-  // }
-  // if (amount < 0 || amount === 'string' || amount === NaN) {
-  //   return `Параметр ${amount} содержит неправильное значение ${amount}`;
-  // } else {
-  //   calculateTotalMortgage();
-  // }
-  // if (date < 0 || date === 'string' || date === NaN) {
-  //   return `Параметр ${date} содержит неправильное значение ${date}`;
-  // } else {
-  //   calculateTotalMortgage();
-  // }
-// }
-  // checkValues();
 function calculateTotalMortgage(percent, contribution, amount, date) {
   if (percent < 0 || isNaN(percent) === true || percent === "") {
     return `Параметр percent содержит неправильное значение ${percent}`;
   } else if (contribution < 0 || isNaN(contribution) === true || contribution === "") {
-    return `Параметр percent содержит неправильное значение ${contribution}`;
+    return `Параметр contribution содержит неправильное значение ${contribution}`;
+  } else if (amount < 0 || isNaN(amount) === true || amount === "") {
+    return `Параметр amount содержит неправильное значение ${amount}`;
+  } else if (new Date > date || date === "") {
+    return `Параметр date содержит неправильное значение ${date}`;
   }
-  // else {
-  //   calculateTotalMortgage();
-  // };
   let P = percent/(12*100);
   let oneMonthImMsec = 31536000000/12;
   let months = Math.round((date.getTime() - new Date().getTime())/oneMonthImMsec);
