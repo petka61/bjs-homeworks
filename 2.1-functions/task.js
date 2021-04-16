00ed41cb32ec1728ea06d97dc8ceac64b13a3fa5
Petr Ivliev <ivliev@inbox.ru>
2021-04-16 23:11:15 +0300

    1st task complited

---
+++ 2.1-functions/task.js
@@ -0,0 +1,32 @@
+'use strict';
+function getSolutions( a, b, c ) {
+  const D = Math.pow(b, 2) - 4*a*c;
+  const newObj = new Object();
+  newObj.D = D;
+  if (D < 0) {
+    newObj.roots = [];
+    return newObj;
+  } else if (D === 0) {
+    const x1 = -b / (2*a);
+    newObj.roots = [x1];
+    return newObj;
+  } else if (D > 0) {
+    const x1 = (-1*b + Math.sqrt(D)) / (2*a);
+    const x2 = (-1*b - Math.sqrt(D)) / (2*a);
+    newObj.roots = [x1, x2];
+    return newObj;
+  }
+}
+
+function showSolutionsMessage(a, b, c) {
+  const result = getSolutions(a, b, c);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(`Значение дискриминанта: ${result.D}`);
+  if (result.D > 0) {
+    console.log(`Уравнение имеет два корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+  }	else if (result.D == 0) {
+    console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else if (result.D < 0) {
+    console.log('Уравнение не имеет вещественных корней');
+  }
+}
