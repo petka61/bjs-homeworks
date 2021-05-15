9a1ae9c8047ac688309ff79f50f64f113f1cf5f3
Petr Ivliev <ivliev@inbox.ru>
2021-05-15 18:42:00 +0300

    tasks completed

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -15,3 +15,41 @@ function validateCount(datas) {
   }
   return parseCount(datas);
 }
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  getPerimeter(a, b, c) {
+    let perimeter = this.a + this.b + this.c;
+    return perimeter;
+  }
+
+  getArea(a, b, c) {
+    let perimeter = this.getPerimeter(a, b, c) / 2;
+    let square = Number((Math.sqrt(perimeter * (perimeter - this.a) * (perimeter - this.b) * (perimeter - this.c))).toFixed(3));
+    return square;
+  }
+}
+
+function getTriangle (a, b, c) {
+  try {
+    const abc = new Triangle(a, b, c);
+    return abc;
+  } catch (e) {
+    return {
+      getArea() {
+        return "Ошибка! Треугольник не существует";
+      },
+      getPerimeter() {
+        return "Ошибка! Треугольник не существует";
+      }
+    };
+  }
+}
